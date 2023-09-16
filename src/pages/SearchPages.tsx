9f07416be43097c52545df75b37aa7f39ae79796
RuslanSolskyi <rus.solskiy@gmail.com>
2023-09-16 19:21:58 +0300

    movies04

--- src/pages/SearchPages.tsx
+++ src/pages/SearchPages.tsx
@@ -4,7 +4,9 @@ import MoviesList from "../components/MoviesContainer/MoviesList";
 
 const SearchPage = () => {
     const dispatch = useAppDispatch();
+
     return (
+
         <div>
             <MoviesList/>
         </div>
