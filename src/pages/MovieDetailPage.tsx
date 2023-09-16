ac028cef282f5cd7a61da3c9e879229b721a6cdb
RuslanSolskyi <rus.solskiy@gmail.com>
2023-09-16 16:37:28 +0300

    movies02

--- src/pages/MovieDetailPage.tsx
+++ src/pages/MovieDetailPage.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const MovieDetailPage = () => {
     return (
         <div>
-            dgfsrkmlmklmklmklmklmklmklmkl
+            dgfsrkmlmklmklmklmklmklmklmklукп
         </div>
     );
 };
